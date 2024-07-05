573c97838fa2804a394461122ab8842a72d1f85d
Ruslan <ssakoks@mail.ru>
2024-07-05 19:31:34 +0500

    add explorer and category screens

--- app/components/screens/category/Category.tsx
+++ app/components/screens/category/Category.tsx
@@ -1,12 +1,26 @@
 import { FC } from 'react'
 import { Text, View } from 'react-native'
 
+import Layout from '@/components/layout/Layout'
+import Loader from '@/components/ui/Loader'
+import Catalog from '@/components/ui/catalog/Catalog'
+
+import { useCategory } from './useCategory'
+
 const Category: FC = () => {
-  return (
-    <View>
-      <Text>Category</Text>
-    </View>
-  )
+	const { category, products, isLoading } = useCategory()
+
+	return isLoading ? (
+		<Loader />
+	) : (
+		<Layout>
+			{category ? (
+				<Catalog title={category.name} products={products || []} />
+			) : (
+				<Text>Category not found</Text>
+			)}
+		</Layout>
+	)
 }
 
 export default Category
--- app/components/screens/explorer/Explorer.tsx
+++ app/components/screens/explorer/Explorer.tsx
@@ -1,12 +1,24 @@
 import { FC } from 'react'
 import { Text, View } from 'react-native'
 
+import Layout from '@/components/layout/Layout'
+import Loader from '@/components/ui/Loader'
+import Catalog from '@/components/ui/catalog/Catalog'
+
+import { useGetAllProducts } from './useGetAllProducts'
+
 const Explorer: FC = () => {
-  return (
-    <View>
-      <Text>Explorer</Text>
-    </View>
-  )
+	const { products, isLoading } = useGetAllProducts()
+
+	return (
+		<Layout>
+			{isLoading ? (
+				<Loader />
+			) : (
+				<Catalog title='Explorer' products={products || []} />
+			)}
+		</Layout>
+	)
 }
 
 export default Explorer
